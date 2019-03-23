fc9a1b77c0dc2e5fa676abf4b733240cf9f526fd
woshinixxk <32725990+woshinixxk@users.noreply.github.com>
2019-03-23 15:00:34 +0800

    Add files via upload

---
+++ 20190316/common.js
@@ -0,0 +1,7 @@
+
+//获取min - max 之间的随机整数
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值 
+}
---
+++ 20190317/common.js
@@ -0,0 +1,4 @@
+// 根据id获取元素
+function my$(id){
+	return document.getElementById(id);
+}
